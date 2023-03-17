09636de8af46127d81a8e36b22bba18176973a2e
Jason Arthur <jason.arthur91@gmail.com>
2023-03-17 12:50:14 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,30 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+// console.log('Hallo kita akan membuat RESTful API');
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+        // credentials: true,
+        // headers: ['Content-Type', 'Authorization'],
+        // preflightStatusCode: 200,
+        // additionalHeaders: ['X-Requested-With'],
+      },
+    },
+  });
+
+  server.route(routes);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
